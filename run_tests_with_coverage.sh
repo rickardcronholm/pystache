02559a9a8c0490a13498b0a2dea2435c38445051
Rickard Cronholm <rickard.cronholm@gmail.com>
2018-11-28 11:41:13 +0000

    Fixed run_tests_with_coverage

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,13 +1,24 @@
-#!/usr/bin/env bash
+PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
+if [ ! -d "venv" ]; then
+	virtualenv venv
+fi
 
-. ~/.virtualenvs/python2.7/bin/activate
+. venv/bin/activate
 
-rm -f pep8.log pyflakes.log
+pip install xmlrunner
+pip install pep8
+pip install pylint
+pip install coverage
 
-PYTHONPATH=. python -m coverage run test.py
+set -e
+set -x
+
+rm -f pep8.log pylint.log
+
+./test.py
 
 python -m coverage xml -o coverage.xml
 python -m coverage html -d coverage
 
 pep8 --max-line-length=120 pystache > pep8.log || true
-pyflakes pystache > pyflakes.log || true
+pylint pystache > pylint.log || true
